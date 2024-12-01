4b637327da4c67726ff5fcef1b82fc69a37e68da
Vinitha Kengatharan <87264797+Vini070@users.noreply.github.com>
2024-12-01 19:32:09 +0530

    Interactive Dice Rolling UI with Randomization

--- dice_roll_application1/lib/main.dart
+++ dice_roll_application1/lib/main.dart
@@ -1,25 +1,72 @@
-import 'package:flutter/material.dart'; 
+import 'dart:math';
+import 'package:flutter/material.dart';
 
-void main(){
-  addTwoNumbers(number2: 5.0,number1: 4.1);
+void main() {
   runApp(const MaterialApp(
-    home: HelloWorldApp(),
+    home: DiceRollApp(),
   ));
 }
 
+class DiceRollApp extends StatefulWidget {
+  const DiceRollApp({super.key});
 
-void addTwoNumbers({required double number1,required double number2}){
-  double answer = number1+number2;
-  print(answer);
+  @override
+  State<DiceRollApp> createState() => _DiceRollAppState();
 }
 
-class HelloWorldApp extends StatelessWidget {
-  const HelloWorldApp({super.key});
+class _DiceRollAppState extends State<DiceRollApp> {
+  String path = 'assets/images/dice-1.png';
 
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
-      body:Center(child: Text('Hello World')),
-      );
+    return Scaffold(
+      backgroundColor: Colors.grey.shade200,
+      body: SizedBox(
+        width: MediaQuery.of(context).size.width,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text(
+              'DICE ROLL APP!',
+              style: TextStyle(
+                  fontSize: 40.0,
+                  fontWeight: FontWeight.w800,
+                  color: Colors.black.withOpacity(0.41)),
+            ),
+            Image.asset(
+              path,
+              height: 225,
+              width: 225,
+            ),
+            GestureDetector(
+              onTap: printRanNum,
+              child: Container(
+                height: 51,
+                width: MediaQuery.of(context).size.width * 0.7,
+                decoration: BoxDecoration(
+                    color: Color.fromARGB(255, 29, 46, 29),
+                    borderRadius: BorderRadius.circular(12.0)),
+                child: Center(
+                  child: Text(
+                    'ROLL DICE',
+                    style: TextStyle(color: Colors.white, fontSize: 20),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  void printRanNum() {
+    int randNumber = Random().nextInt(6) + 1;
+    // print('the image path is $path');
+    setState(() {
+      path = 'assets/images/dice-$randNumber.png';
+    });
+    // print('the new image path is $path');
   }
 }
